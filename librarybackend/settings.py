29de1d8304502718e0cc87b7a663ebb0569617df
Stephen Appiah <stephenappiaa@gmail.com>
2026-07-30 15:53:13 +0000

    debug to false

--- librarybackend/settings.py
+++ librarybackend/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config("DEBUG", default=False, cast=bool)
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS_ENV = config("ALLOWED_HOSTS", default="localhost,127.0.0.1,0.0.0.0,library-backend-a3sj.onrender.com")
 ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS_ENV.split(",")]
