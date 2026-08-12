da3c30926b1d19711f26278a339882b88ffb49e8
Stephen Appiah <stephenappiaa@gmail.com>
2026-08-12 20:45:56 +0000

    database

--- librarybackend/settings.py
+++ librarybackend/settings.py
@@ -89,14 +89,20 @@ WSGI_APPLICATION = "librarybackend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
-DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
+DATABASES = {
+    "default": dj_database_url.parse(
+        config("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
 
 
 # Password validation
